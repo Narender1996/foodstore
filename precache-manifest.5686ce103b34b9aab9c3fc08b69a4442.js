642db66b6c9be79c6d43cef748fe275549121d82
narender bisht <narender007bisht@gmail.com>
2020-11-15 07:42:28 -0800

    Updates

---
+++ precache-manifest.5686ce103b34b9aab9c3fc08b69a4442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cd2cf0446963659a7ecddba84b9751",
+    "url": "/foodstore/index.html"
+  },
+  {
+    "revision": "cd8be21c3be0f770fb29",
+    "url": "/foodstore/static/css/main.62532236.chunk.css"
+  },
+  {
+    "revision": "6e8561d5b5ba3f313e27",
+    "url": "/foodstore/static/js/2.8dccb73d.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/foodstore/static/js/2.8dccb73d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8be21c3be0f770fb29",
+    "url": "/foodstore/static/js/main.7f89108a.chunk.js"
+  },
+  {
+    "revision": "90520891f299a47846e6",
+    "url": "/foodstore/static/js/runtime-main.2b3e97da.js"
+  }
+]);
