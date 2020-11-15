bb82b6c91a5ef25030c31d5e4812bedeba972749
narender bisht <narender007bisht@gmail.com>
2020-11-15 08:00:10 -0800

    first change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {BrowserRouter,Switch,Route} from 'react-router-dom';
 function App() {
   return (
     
-    <BrowserRouter>
+    <BrowserRouter basename="src">
     <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/addproduct" component={Addproduct} />
